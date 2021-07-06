a18e0f0e76245c8d17b700ebc463ee436639c5d6
aynul-abir <aynulabir51@example.com>
2021-07-06 23:24:29 +0600

    First commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from .views import checkout, HomeView, ItemDetailView
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
+    path('checkout/', checkout, name="checkout"),
+    path('product/<slug>/', ItemDetailView.as_view(), name="product"),
+]
---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Item
+
+# Create your views here.
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = 'home-page.html'
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product-page.html'
+
+def checkout(request):
+    return render(request, "checkout-page.html")
+
+
+#def product(request):
+ #   return render(request, "product-page.html")
+
